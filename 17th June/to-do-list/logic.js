2f70fd9ef3239b50f4590477d8caf764ef9f8b44
= <=abhishekzobin02052000@gmail.com>
2022-06-18 16:33:38 +0530

    Completed To do List

--- 17th June/to-do-list/logic.js
+++ 17th June/to-do-list/logic.js
@@ -1,97 +1,22 @@
-window.onload = () => {
-    const form1 = document.querySelector("#addForm");
-
-    let items = document.getElementById("items");
-    let submit = document.getElementById("submit");
-
-    let editItem = null;
-
-    form1.addEventListener("submit", addItem);
-    items.addEventListener("click", removeItem);
-};
-
-function addItem(e) {
-    e.preventDefault();
-
-    if (submit.value != "Submit") {
-        console.log("Hello");
-
-        editItem.target.parentNode.childNodes[0].data
-            = document.getElementById("item").value;
-
-        submit.value = "Submit";
-        document.getElementById("item").value = "";
-
-        document.getElementById("lblsuccess").innerHTML
-            = "Text edited successfully";
-
-        document.getElementById("lblsuccess")
-            .style.display = "block";
-
-        setTimeout(function () {
-            document.getElementById("lblsuccess")
-                .style.display = "none";
-        }, 3000);
-
-        return false;
+const bttN = document.getElementById("submit");
+const newDiv = document.createElement("div");
+const boDy=document.getElementById("body");
+bttN.addEventListener("click",myFuc);
+boDy.append(newDiv);
+const input=document.getElementById("takingInput");
+function myFuc(){
+    if(String(input.value).length==0){
+        console.log("I am here");
+        console.log(String(input.value).length);
+        alert("Please provide some input");
+        return;
     }
 
-    let newItem = document.getElementById("item").value;
-    if (newItem.trim() == "" || newItem.trim() == null)
-        return false;
-    else
-        document.getElementById("item").value = "";
-
-    let li = document.createElement("li");
-    li.className = "list-group-item";
-
-    let deleteButton = document.createElement("button");
-
-    deleteButton.className =
-        "btn-danger btn btn-sm float-right delete";
-
-    deleteButton.appendChild(document.createTextNode("Delete"));
-
-    let editButton = document.createElement("button");
-
-    editButton.className =
-        "btn-success btn btn-sm float-right edit";
-
-    editButton.appendChild(document.createTextNode("Edit"));
-
-    li.appendChild(document.createTextNode(newItem));
-    li.appendChild(deleteButton);
-    li.appendChild(editButton);
-
-    items.appendChild(li);
-}
-
-function removeItem(e) {
-    e.preventDefault();
-    if (e.target.classList.contains("delete")) {
-        if (confirm("Are you Sure?")) {
-            let li = e.target.parentNode;
-            items.removeChild(li);
-            document.getElementById("lblsuccess").innerHTML
-                = "Text deleted successfully";
-
-            document.getElementById("lblsuccess")
-                .style.display = "block";
-
-            setTimeout(function () {
-                document.getElementById("lblsuccess")
-                    .style.display = "none";
-            }, 3000);
-        }
-    }
-    if (e.target.classList.contains("edit")) {
-        document.getElementById("item").value =
-            e.target.parentNode.childNodes[0].data;
-        submit.value = "EDIT";
-        editItem = e;
-    }
-}
-
-function toggleButton(ref, btnID) {
-    document.getElementById(btnID).disabled = false;
+    const newDiv1=document.createElement("div");
+    newDiv1.innerHTML=`${input.value}
+    <input type="submit" value="edit">
+    <input type="submit" value="delete">
+    `
+    input.value="";
+    newDiv.append(newDiv1);
 }
